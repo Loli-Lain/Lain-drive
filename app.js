fa0d22e05bb7ca0c0fc10e52be17c253faaa012a
Zyy955 <1072411694@qq.com>
2024-01-28 07:39:56 +0800

    :tada:

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-import { exec } from 'child_process'
-import { encode as encodeSilk } from 'silk-wasm'
-import { randomUUID } from 'crypto'
+import { exec as execCb } from 'child_process'
+import crypto from 'crypto'
 import express from 'express'
 import { fileTypeFromBuffer } from 'file-type'
 import fs from 'fs'
 import http from 'http'
 import sizeOf from 'image-size'
 import multer from 'multer'
+import { encode as encodeSilk } from 'silk-wasm'
+import { promisify } from 'util'
 import Cfg from './lib/config.js'
 import './lib/init.js'
+
+const exec = promisify(execCb)
 
 class Server {
   constructor () {
@@ -23,135 +26,160 @@ class Server {
 
     /** 处理multipart/form-data请求 */
     const upload = multer({ storage: multer.memoryStorage() })
+    /** 设置静态路径 */
+    app.use('/static', express.static(process.cwd() + '/data'))
 
     /** POST请求 接收文件 */
     app.post('/api/upload', upload.single('file'), async (req, res) => {
       const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null
-      const Token = req.headers.token || null
-      const type = req.headers.type
-      let silk = req.headers.silk
-      silk = silk === 'undefined' ? undefined : (silk === 'false' ? false : silk)
-      if (!req.headers['content-type'].startsWith('multipart/form-data')) {
-        logger.error(`[请求格式错误][POST]  [IP：${ip}] => [Token：${Token}] => [返回：已拒绝]`)
-        res.status(400).json({ status: 'failed', message: '请求格式错误' })
-      }
+      const token = req.headers.token || null
 
-      logger.info(`[收到请求][POST] [IP：${ip}] => [Token：${Token}]`)
+      logger.info(`<post:接收> -> <ip:${ip}> -> <token:${token}>`)
 
-      if (!Token || Token !== Cfg.token) {
-        logger.error(`[请求Token错误][POST]  [IP：${ip}] => [Token：${Token}] => [返回：已拒绝]`)
+      if (!token || token !== Cfg.token) {
+        logger.error(`<post:返回> => <ip:${ip}> => <token:${token}> => <message:token错误>`)
         return res.status(500).json({ status: 'failed', message: 'token错误' })
       }
 
-      try {
-        /** 获取buffer */
-        let { buffer, size, originalname } = req.file
-        /** 生成一个临时token */
-        const UUID = randomUUID()
+      /** 是否符合要求 */
+      const fileType = req.file ? 'file' : req.body.mp3 ? 'mp3' : req.body.link ? 'link' : null
+      if (!fileType) {
+        logger.error(`<post:返回> => <ip:${ip}> => <token:${token}> => <message:文件类型错误>`)
+        return res.status(400).json({ error: '文件类型错误' })
+      }
 
-        let extension
-        let contentType
-
-        /** 检查请求头，是否为云转码的语音 */
-        if (silk) {
-          const FileAudio = await this.getAudio(buffer)
-          if (FileAudio.ok) {
-            buffer = FileAudio.data
-            extension = 'silk'
-            contentType = 'audio/silk'
-          } else {
-            res.status(500).json({ status: 'failed', message: '云转码失败' })
-            logger.error(`[错误][POST]  [IP：${ip}] => [Token：${Token}] => [返回：云转码失败]`)
-            return logger.error(FileAudio.data)
-          }
-        } else if (type === 'audio') {
-          extension = 'silk'
-          contentType = 'audio/silk'
-        } else {
-          /** 获取文件类型，响应头 */
-          const FileType = await this.getType(buffer)
-          extension = FileType.FileType
-          contentType = FileType.contentType
-        }
-
-        /** 先赋值 */
-        let data = {
-          status: 'ok',
-          buffer,
-          File: {
-            size,
-            extension,
-            token: UUID,
-            originalname,
-            contentType,
-            url: Cfg.baseUrl.replace(/\/$/, '') + `/api/File?token=${UUID}`
-          }
-        }
-
-        /** 如果是图片，计算一下宽高 */
-        if (contentType.includes('image')) {
-          const image = sizeOf(buffer)
-          data.File = { ...data.File, ...image }
-        }
-
-        /** 保存 */
-        this.File.set(UUID, data)
-        /** 定时删除 */
-        setTimeout(() => {
-          this.File.delete(UUID)
-          logger.debug(`[定时任务] [删除过期文件] => [Key：${UUID}]`)
-        }, (Cfg.ExpirationTime || 30) * 1000)
-        logger.info(`[完成请求][POST] [IP：${ip}] => [Token：${Token}] => [Key：${UUID}]`)
-        logger.debug(data.File)
-        res.status(200).json(data.File)
-      } catch (error) {
-        res.status(500).json({ status: 'failed', message: '哎呀，报错了捏' })
-        logger.error(`[未知错误][POST]  [IP：${ip}] => [Token：${Token}] => [返回：未知错误]`)
-        logger.error(error)
+      switch (fileType) {
+        /** 文件，直接存本地即可 */
+        case 'file':
+          return await this.returnPost(res, token, ip, req.file.buffer)
+        /** 通过此接口上传的，均为需要转码 */
+        case 'mp3':
+          return await this.MP3File(res, token, ip, req.file.buffer)
+        /** 传递link，使用此配置，请求头必须设置文件类型 支持file、mp3，图片视频和silk均可传递file */
+        case 'link':
+          if (!req.headers.type) return res.status(400).json({ error: '未传递文件类型' })
+          return await this.linkFile(res, token, ip, req.body.link, req.headers.token)
+        default:
+          logger.error(`<post:返回> => <ip:${ip}> => <token:${token}> => <message:文件类型错误>`)
+          return res.status(400).json({ error: '文件类型错误' })
       }
     })
 
     /** Get请求 返回文件 */
-    app.get('/api/File', async (req, res) => {
+    app.get('/api/File/:filename', async (req, res) => {
+      const File = process.cwd() + `/temp/${req.params.filename}`
       const ip = req.ip
       const { token } = req.query
 
-      logger.info(`[收到请求][GET] [IP：${ip}] => [Token：${token || JSON.stringify(req.query)}]`)
+      logger.info(`<get:接收> -> <ip:${ip}> -> <token:${token}>`)
 
-      try {
-        /** 提取对应的文件 */
-        const data = this.File.get(token)
-        if (data) {
-          res.setHeader('Content-Type', data?.File?.contentType || 'image/png')
-          res.setHeader('Content-Disposition', 'inline')
-          res.send(data.buffer)
-          logger.info(`[请求完成][GET] [IP：${ip}] => [Token：${token}] => [文件：${data?.File?.contentType}]`)
-          logger.debug(data.File)
-        } else {
-          res.setHeader('Content-Type', 'image/png')
-          res.setHeader('Content-Disposition', 'inline')
-          res.send(fs.readFileSync(Cfg.File404))
-          logger.info(`[请求完成][GET] [IP：${ip}] => [Token：${token}] => [文件：${Cfg.File404}]`)
-        }
-      } catch (error) {
-        res.status(500).json({ status: 'failed', message: '哎呀，报错了捏' })
-        logger.error(`[未知错误][GET]  [IP：${ip}] => [Token：${token}] => [返回：未知错误]`)
-        logger.error(error)
+      /** token错误 */
+      if (!token || token !== Cfg.token) {
+        logger.error(`<get:返回> => <ip:${ip}> => <token:${token}> => <message:token错误>`)
+        return res.status(500).json({ status: 'failed', message: 'token错误' })
       }
+
+      /** 文件不存在 */
+      if (!fs.existsSync(File)) {
+        logger.error(`<get:返回> => <ip:${ip}> => <token:${token}> => <message:文件不存在>`)
+        return res.status(400).json({ error: '文件不存在' })
+      }
+
+      /** 设置响应头 */
+      res.setHeader('Content-Type', 'application/octet-stream')
+      res.setHeader('Content-Disposition', 'inline')
+      logger.mark(logger.green(`<get:返回> => <ip:${ip}> => <File:${File}>`))
+      return fs.createReadStream(File).pipe(res)
     })
 
     http.createServer(app, '0.0.0.0').listen(Cfg.port, () => logger.info(`HTTP服务器已启动：${Cfg.baseUrl || `http://127.0.0.1:${Cfg.port})`}`))
   }
 
-  /** 获取文件后缀和Content-Type */
+  /** post请求成功返回 */
+  async returnPost (res, token, ip, buffer) {
+    try {
+      /** 取文件基本信息 */
+      const data = await this.FileData(buffer)
+      const _path = `./temp/${data.name}`
+      /** 本地没有就保存 */
+      if (!fs.existsSync(_path)) fs.writeFileSync(_path, buffer)
+      data.url = Cfg.baseUrl.replace(/\/$/, '') + `/api/File/${data.name}?token=${token}`
+      logger.mark(logger.green(`<post:返回> => <ip:${ip}> => <link:${data.url}>`))
+      return res.status(200).json(data)
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ status: 'failed', message: '未知错误' })
+    }
+  }
+
+  /** MP3 */
+  async MP3File (res, token, ip, buffer) {
+    try {
+      /** 先通过名称看下是否已存在转码好的 */
+      const mp3 = await this.FileData(buffer)
+      const _path = `./temp/${mp3.name}`
+      /** 对于语音，如果本地有就直接返回，没有就需要转码 */
+      if (fs.existsSync(_path)) {
+        mp3.url = Cfg.baseUrl.replace(/\/$/, '') + `/api/File/${mp3.name}?token=${token}`
+        logger.mark(logger.green(`<post:返回> => <ip:${ip}> => <link:${mp3.url}>`))
+        return res.status(200).json(mp3)
+      }
+      /** 转码 */
+      const { ok, data } = await this.getAudio(buffer)
+      if (!ok) {
+        logger.error(`<post:返回> => <ip:${ip}> => <token:${token}> => <message:转码失败>`)
+        return res.status(400).json({ error: '转码失败' })
+      }
+      /** 存一份MP3的md5 下次可通过mp3的md5拿到转码好的 */
+      fs.writeFileSync(_path, data)
+      /** 随后正常返回即可 */
+      return await this.returnPost(res, token, ip, data)
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ status: 'failed', message: '未知错误' })
+    }
+  }
+
+  /** link */
+  async linkFile (res, token, ip, link, type) {
+    try {
+      /** 先下载成为buffer */
+      link = Buffer.from(await (await fetch(link)).arrayBuffer())
+      if (type === 'mp3') return await this.MP3File(res, token, ip, link)
+      return await this.returnPost(res, token, ip, link)
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ status: 'failed', message: '未知错误' })
+    }
+  }
+
+  /** 计算文件名称 */
+  async FileData (buffer) {
+    const size = buffer.length
+    const md5 = crypto.createHash('md5').update(buffer).digest('hex')
+    let image = { width: 0, height: 0 }
+    try { image = sizeOf(buffer) } catch { }
+    const { width, height } = image
+    const { mime, ext } = await this.getType(buffer)
+    const name = `${md5}-${size}-${width}-${height}-${mime}.${ext}`
+
+    return {
+      size,
+      md5,
+      width,
+      height,
+      mime,
+      ext,
+      name
+    }
+  }
+
+  /** 获取文件后缀、mime */
   async getType (buffer) {
     try {
-      const { mime, ext } = await fileTypeFromBuffer(buffer)
-      return { extension: ext, contentType: mime }
+      return await fileTypeFromBuffer(buffer)
     } catch (error) {
-      logger.error('获取格式错误，默认返回txt')
-      logger.debug(error)
-      return { extension: 'txt', contentType: 'application/octet-stream' }
+      return { mime: 'application/octet-stream', ext: 'txt' }
     }
   }
 
@@ -179,6 +207,7 @@ class Server {
       fs.unlink(pcm, () => { })
       return { ok: true, data }
     } catch (error) {
+      logger.error(error)
       return { ok: false, data: error }
     }
   }
@@ -186,28 +215,15 @@ class Server {
   /** ffmpeg转码 转为pcm */
   async runFfmpeg (input, output) {
     let cm
-    let ret = await new Promise((resolve, reject) => exec('ffmpeg -version', { windowsHide: true }, (error, stdout, stderr) => resolve({ error, stdout, stderr })))
-    return new Promise((resolve, reject) => {
-      if (ret.stdout) {
-        cm = 'ffmpeg'
-      } else {
-        cm = Cfg.ffmpeg_path || null
-      }
-
-      if (!cm) {
-        throw new Error('未检测到 ffmpeg ，无法进行转码，请正确配置环境变量或手动前往 config.yaml 进行配置')
-      }
-
-      exec(`${cm} -i "${input}" -f s16le -ar 48000 -ac 1 "${output}"`, async (error, stdout, stderr) => {
-        if (error) {
-          logger.error(`执行错误: ${error}`)
-          reject(error)
-          return
-        }
-        resolve()
-      }
-      )
-    })
+    const { stdout } = await exec('ffmpeg -version', { windowsHide: true })
+    cm = stdout ? 'ffmpeg' : (Cfg.ffmpeg_path || null)
+    if (!cm) throw new Error('未检测到 ffmpeg ，无法进行转码，请正确配置环境变量或手动前往 config.yaml 进行配置')
+    try {
+      await exec(`${cm} -i "${input}" -f s16le -ar 48000 -ac 1 "${output}"`)
+    } catch (error) {
+      logger.error(`执行错误: ${error}`)
+      throw error
+    }
   }
 }
 
